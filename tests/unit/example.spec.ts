0705339eb720105914b210d49d95812b7a70c4ce
Aaron Saunders <aaronksaunders@gmail.com>
2021-06-01 20:10:00 -0400

    added missing imports

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import Folder from '@/views/Folder.vue'
+import Folder from '@/views/Other.vue'
 
 describe('Folder.vue', () => {
   it('renders folder view', () => {
